2908f75e7ff464b8da8adaac2b9dcbef97ad1535
AlejandroAlv12 <00211623@uca.edu.sv>
2024-07-31 17:30:14 -0600

    google maps

---
+++ js/social_media.js
@@ -0,0 +1,13 @@
+
+(function(){
+    const instagram =document.querySelector('.instagram');
+    const facebook=document.querySelector('.facebook');
+
+    instagram.addEventListener('click' , ()=>{
+        alert('Coming soon...');
+    });
+    facebook.addEventListener('click' , ()=>{
+        alert('Coming soon...');
+    });
+
+})();
