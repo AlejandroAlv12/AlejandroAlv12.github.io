18e6011816c90e63b4802373b8fafdc50a33710d
AlejandroAlv12 <00211623@uca.edu.sv>
2025-06-07 22:04:43 -0600

    fix: corregir error de alert en instagram

--- js/social_media.js
+++ js/social_media.js
@@ -1,12 +1,8 @@
 
 (function(){
-    const instagram =document.querySelector('.instagram');
     const twitter=document.querySelector('.Xlogo');
     
 
-    instagram.addEventListener('click' , ()=>{
-        alert('Coming soon...');
-    })
     twitter.addEventListener('click' , ()=>{
         alert('Coming soon...');
     });
