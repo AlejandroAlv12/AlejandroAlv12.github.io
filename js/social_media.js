ab3f222d851ea9156685a8614d9221cafbee25ef
AlejandroAlv12 <00211623@uca.edu.sv>
2025-06-07 21:45:20 -0600

    feat: agregar red social instagram

--- js/social_media.js
+++ js/social_media.js
@@ -1,16 +1,12 @@
 
 (function(){
     const instagram =document.querySelector('.instagram');
-    const facebook=document.querySelector('.facebook');
     const twitter=document.querySelector('.Xlogo');
     
 
     instagram.addEventListener('click' , ()=>{
         alert('Coming soon...');
-    });
-    facebook.addEventListener('click' , ()=>{
-        alert('Coming soon...');
-    });
+    })
     twitter.addEventListener('click' , ()=>{
         alert('Coming soon...');
     });
